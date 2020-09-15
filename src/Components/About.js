884538d65591b30e0cba18a74f16bff1f8b81e72
Jason Sio <son8292003@gmail.com>
2020-09-15 16:45:07 +0000

    update cms file path and header to remove project button

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {BASEURL} from '../Configs/Apis';
+
 
 class About extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic= BASEURL + this.props.data.image.url;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -13,7 +15,7 @@ class About extends Component {
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+      var resumeDownload = BASEURL + this.props.data.resumedownload.url;
     }
 
     return (
